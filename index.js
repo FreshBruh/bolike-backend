b96a465bc349021fdbc4d30566d341362e570a46
FreshBruh <upsetzombiegaming@gmail.com>
2023-04-26 17:29:22 +0200

    Add node modules

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.post('/', (req, res) => {
+//   console.log('Got body:', req.body);
+//   res.sendStatus(200);
+// });
+
+app.get('/', (req,res) => {
+  res.send('Hello world');
+})
 
 app.listen(50000, () => {
   console.log('Server listening on port 50000');
