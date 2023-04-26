2e3f7ffc10871e17401f696f2f257f8c496980ec
FreshBruh <upsetzombiegaming@gmail.com>
2023-04-26 17:33:30 +0200

    Add cors policy

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'https://localhost'
+}));
+
 
 // app.post('/', (req, res) => {
 //   console.log('Got body:', req.body);
